56388a44f75d044ed2fa50f411613b5bfd88faf1
ejj28 <eriksinventions11@gmail.com>
2020-07-06 16:04:16 -0400

    Add prototype metadata code

--- src/playback.js
+++ src/playback.js
@@ -1,4 +1,18 @@
 var howler = require('howler');
+const mm = require('music-metadata');
+const util = require('util');
+ 
+mm.parseFile('src/test.flac')
+  .then( metadata => {
+    console.log(util.inspect(metadata.common, { showHidden: false, depth: null }));
+    $("#controlBarAlbumArt").attr("src",`data:${metadata.common.picture[0].format};base64,${metadata.common.picture[0].data.toString('base64')}`);
+    $("#controlBarTitle").text(metadata.common.title)
+    $("#controlBarArtist").text(metadata.common.artist)
+  })
+  .catch( err => {
+    console.error(err.message);
+  });
+
 
 var sound = new howler.Howl({
     src: ['test.flac']
@@ -6,7 +20,7 @@ var sound = new howler.Howl({
 
 var isPlaying = false;
 
-$("#seekBar").bootstrapSlider('option',{min: 0, max: Math.round(sound.duration())});
+//$("#seekBar").bootstrapSlider('option',{min: 0, max: Math.round(sound.duration())});
 
 $('#playpause').click(() => {
     console.log("click")
@@ -21,10 +35,10 @@ $('#playpause').click(() => {
     }
 });
 
-setInterval(() => {
+/*setInterval(() => {
     updateBar();
 }, 250);
-
+*/
 function updateBar() {
     $("#seekBar").bootstrapSlider('value',Math.round(sound.seek()));
 }
