52086a39762c59eb700c4ac4a6b2014cba071720
ejj28 <eriksinventions11@gmail.com>
2020-07-28 12:23:31 -0400

    Playback bar CSS tweaks

--- src/playback.js
+++ src/playback.js
@@ -1,22 +1,22 @@
 var howler = require('howler');
 const mm = require('music-metadata');
 const util = require('util');
- 
+
 mm.parseFile('src/test.flac')
-  .then( metadata => {
-    console.log(util.inspect(metadata.common, { showHidden: false, depth: null }));
-    $("#controlBarAlbumArt").attr("src",`data:${metadata.common.picture[0].format};base64,${metadata.common.picture[0].data.toString('base64')}`);
-    $("#controlBarTitle").text(metadata.common.title)
-    $("#controlBarArtist").text(metadata.common.artist)
-  })
-  .catch( err => {
-    console.error(err.message);
-  });
+    .then(metadata => {
+        console.log(util.inspect(metadata.common, { showHidden: false, depth: null }));
+        $("#controlBarAlbumArt").attr("src", `data:${metadata.common.picture[0].format};base64,${metadata.common.picture[0].data.toString('base64')}`);
+        $("#controlBarTitle").text(metadata.common.title)
+        $("#controlBarArtist").text(metadata.common.artist)
+    })
+    .catch(err => {
+        console.error(err.message);
+    });
 
 
 var sound = new howler.Howl({
     src: ['test.flac']
-  });
+});
 
 var isPlaying = false;
 
@@ -29,9 +29,13 @@ $('#playpause').click(() => {
     if (!isPlaying) {
         sound.play();
         isPlaying = true;
+        $("#playpause-icon").removeClass("fa-play");
+        $("#playpause-icon").addClass("fa-pause");
     } else {
         sound.pause();
         isPlaying = false;
+        $("#playpause-icon").removeClass("fa-pause");
+        $("#playpause-icon").addClass("fa-play");
     }
 });
 
@@ -40,5 +44,5 @@ $('#playpause').click(() => {
 }, 250);
 */
 function updateBar() {
-    $("#seekBar").bootstrapSlider('value',Math.round(sound.seek()));
+    $("#seekBar").bootstrapSlider('value', Math.round(sound.seek()));
 }
